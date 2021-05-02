cd643c0c35e7f299528f57f2f3df2c8e35e09010
Tanner Stratford <57194030+tannerstratford@users.noreply.github.com>
2021-05-01 21:59:50 -0600

    prove02

---
+++ cse341-project-master/Prove02/routes/prove02-routes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const rootDir = require('../../util/path');
+const router = express.Router();
+const bookTitle = [];
+const bookSummary = [];
+
+router.post('/add-books', (req, res, next) => {
+    const title = req.body.bookTitle;
+    const summary = req.body.bookSummary;
+
+    bookTitle.push(title);
+    bookSummary.push(summary);
+
+    res.redirect('/');
+})
+
+router.get('/show-book', (req, res, next) => {
+    res.render('pages/show-book', {
+        title: 'Prove 02 assignment', 
+        path: '/show-book', 
+        bookTitle: bookTitle,
+        bookSummary: bookSummary, 
+        activeTA03: true, 
+        contentCSS: true
+    });
+});
+
+router.get('/', (req, res, next) => {
+    res.render('pages/add-book', {
+        title: 'Prove 02 assignment', 
+        path: '/',  
+        activeTA03: true, 
+        contentCSS: true,
+    
+    });
+});
+exports.routes = router;
